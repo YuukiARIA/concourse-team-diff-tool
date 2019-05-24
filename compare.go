a92dcf21fa7ba4859385c06528aae7b401b2ca58
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-05-24 14:41:38 +0900

    Implament compare logic

--- compare.go
+++ compare.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 
+	"github.com/fatih/color"
+
 	"github.com/YuukiARIA/concourse-team-diff-tool/models"
+	"github.com/YuukiARIA/concourse-team-diff-tool/stringSet"
+)
+
+var (
+	colorOfCreated  = color.New(color.FgGreen)
+	colorOfDeleted  = color.New(color.FgRed)
+	colorOfRetained = color.New(color.FgWhite)
 )
 
 func Compare(oldTeam, newTeam models.Team) {
 	for roleName, oldRule := range oldTeam.Auth {
 		newRule, exists := newTeam.Auth[roleName]
 		if exists {
+			fmt.Println("role: " + roleName)
 			compareRule(oldRule, newRule)
 		} else {
 			fmt.Println("role definition deleted: " + roleName)
@@ -24,5 +34,35 @@ func Compare(oldTeam, newTeam models.Team) {
 }
 
 func compareRule(oldRule, newRule models.AuthRule) {
-	// TODO
+	fmt.Println("users:")
+	compareIds(oldRule.Users, newRule.Users)
+	fmt.Println("groups:")
+	compareIds(oldRule.Groups, newRule.Groups)
+}
+
+func compareIds(oldIds, newIds []string) {
+	oldIdsSet, newIdsSet := stringSet.New(oldIds...), stringSet.New(newIds...)
+	deletedIdsSet, createdIdsSet, retainedIdsSet := stringSet.Partition(oldIdsSet, newIdsSet)
+
+	showAsCreated(createdIdsSet.Array())
+	showAsDeleted(deletedIdsSet.Array())
+	showAsRetained(retainedIdsSet.Array())
+}
+
+func showAsCreated(values []string) {
+	showWithColor(values, "+", colorOfCreated)
+}
+
+func showAsDeleted(values []string) {
+	showWithColor(values, "-", colorOfDeleted)
+}
+
+func showAsRetained(values []string) {
+	showWithColor(values, " ", colorOfRetained)
+}
+
+func showWithColor(values []string, prefix string, color *color.Color) {
+	for _, value := range values {
+		color.Printf("%s %s\n", prefix, value)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/YuukiARIA/concourse-team-diff-tool
 
 go 1.12
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+)
