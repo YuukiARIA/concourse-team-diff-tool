2db07dd3f60aa257fb1d021c5212abee9eb84f44
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-05-27 19:00:02 +0900

    Simplify loadTextFromFile

--- main.go
+++ main.go
@@ -26,13 +26,7 @@ const (
 )
 
 func loadTextFromFile(filePath string) ([]byte, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	return ioutil.ReadAll(file)
+	return ioutil.ReadFile(filePath)
 }
 
 func loadTextFromReader(reader io.Reader) ([]byte, error) {
